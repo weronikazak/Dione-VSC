d2a0c64fa4a0fefd1fdafed6491022c0e7f27e24
weronikazak <weronikarzak@gmail.com>
2023-10-11 15:05:33 +0100

    Add right mouse click

--- extension.js
+++ extension.js
@@ -1,5 +1,7 @@
 const vscode = require('vscode');
 const retrieveConfluencePages = require('./retrieve-pages');
+const fs = require('fs');
+const path = require('path');
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -26,6 +28,30 @@ function activate(context) {
 			console.log('response ' + response);
 		});
 	});
+
+	const appendText = vscode.commands.registerCommand('dione.copyAndDocument', () => {
+        const editor = vscode.window.activeTextEditor;
+
+        if (editor) {
+            const selection = `
+<code>
+	${editor.document.getText(editor.selection)}
+</code>`;
+			
+			if (editor.document.getText(editor.selection)) {
+                // You can customize the file path and name here
+                const newFileName = 'selectedText.md';
+                const newFilePath = path.join(vscode.workspace.workspaceFolders[0].uri.fsPath, newFileName);
+				
+                fs.writeFileSync(newFilePath, selection);
+                vscode.workspace.openTextDocument(newFilePath).then(doc => {
+                    vscode.window.showTextDocument(doc);
+                });
+            }
+        }
+    });
+
+    context.subscriptions.push(appendText);
 
 	context.subscriptions.push(disposable);
 	context.subscriptions.push(vscode.commands.registerCommand('dione.listPages', () => {retrieveConfluencePages(context)}));
