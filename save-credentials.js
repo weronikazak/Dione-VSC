e7c87a46217a1f49cc09620f893f4d87dbbe9626
weronikazak <weronikarzak@gmail.com>
2023-10-24 13:44:10 +0100

    Save credentials update

--- save-credentials.js
+++ save-credentials.js
@@ -30,9 +30,30 @@ const saveCredentials = async function retrieveConfluencePages(context) {
     });
     return;
   }
+
+  const openApi = await vscode.window.showInputBox({
+    placeHolder: "Enter your OpenAI API key",
+    prompt: "Enter your  OpenAI API key",
+  });
+
+  if (!openApi) {
+    // create an error message with buttons to external website
+    vscode.window.showInformationMessage(
+      "You must enter your Open API token",
+      ...["Get API Token"]
+    ).then((selection) => {
+      if (selection === "Get API Token") {
+        vscode.env.openExternal(vscode.Uri.parse("https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key"));
+      }
+    });
+    return;
+  }
   
   context.globalState.update("EMAIL", email);
   context.globalState.update("API_TOKEN", apiToken);
+  context.globalState.update("OPENAI_API_KEY", openApi);
+
+  vscode.window.showInformationMessage("Credentials saved successfully!");
 };
 
 module.exports = saveCredentials;
